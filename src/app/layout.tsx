79004020f28ddd7e06f44b53275624c86d5d6a37
Abe Mpesa <gitongaevans77@gmail.com>
2026-01-30 14:46:15 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         <script src="https://cdn.jsdelivr.net/npm/chart.js" async></script>
       </head>
-      <body>
+      <body suppressHydrationWarning={true}>
         <AuthProvider>
           {children}
           <Toaster />
