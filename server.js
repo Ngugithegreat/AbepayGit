214728cb450508f0be6d2cbe267b666707cf9393
Abe Mpesa <gitongaevans77@gmail.com>
2026-02-01 19:12:31 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { createServer } = require('http');
+const { parse } = require('url');
 const next = require('next');
 
 const port = 5000;
@@ -9,7 +10,8 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    handle(req, res);
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
   }).listen(port, hostname, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
