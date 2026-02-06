8b01c03a0747e16ed7b9890aea8f3e7dd9c71e63
Abe Mpesa <gitongaevans77@gmail.com>
2026-02-06 08:19:20 +0000

    when i click login & link with deriv, i get this error page on the scree

---
+++ server.js
@@ -0,0 +1,20 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port = 5000;
+const hostname = '0.0.0.0';
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, hostname, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
